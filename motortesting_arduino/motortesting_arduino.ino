2b75573404a10928f62b27dcf02b6ae1bec3d597
Andreas Muehlmann <andi.muehlmann@arcor.de>
2023-04-13 00:38:27 +0200

    measuring rpm with arduino

--- motortesting_arduino/motortesting_arduino.ino
+++ motortesting_arduino/motortesting_arduino.ino
@@ -1,12 +1,38 @@
-long randNumber;
+int SENSORPIN = 3;
+int HOLES = 40;
+
+volatile unsigned long count, t2;
+unsigned long t1;
+double rpm;
+
+void interrupt_handler() {
+    count += 1;
+    t2 = millis();
+}
 
 void setup() {
-  Serial.begin(9600);
-  randomSeed(analogRead(0));
+    Serial.begin(9600);
+    count = 0;
+    t1 = 0;
+    t2 = 0;
+    rpm = 0;
+    pinMode(SENSORPIN, INPUT);
+    attachInterrupt(digitalPinToInterrupt(SENSORPIN), interrupt_handler, FALLING);
 }
 
 void loop() {
-  randNumber = random(7) - 3;
-  Serial.println(randNumber);
-  delay(5);
+    double T_in_micro_s;
+    if (t2 > t1) {
+        T_in_micro_s = HOLES * (t2 - t1) / count;
+        rpm = (unsigned) (long) (60000 / T_in_micro_s);
+        t1 = t2;
+        count = 0;
+    } else {
+        T_in_micro_s = HOLES * (millis() - t1);
+        double theoretical_rpm = (unsigned) (long) (60000 / T_in_micro_s);
+        if (theoretical_rpm < rpm) {
+          rpm = theoretical_rpm;
+        }
+    }
+    Serial.println(rpm);
 }
--- serial_interface.py
+++ serial_interface.py
@@ -1,14 +1,17 @@
 import serial
+import time
 
 
 class Serial_Interface:
     def __init__(self):
         self.ser = serial.Serial('/dev/ttyUSB0', 9600)
-        pass
+        time.sleep(2)
 
     def recv_data(self):
         return self.ser.readline().decode().strip()
 
-
     def send(self, data):
         pass
+    
+    def reset(self):
+        self.ser.close()
