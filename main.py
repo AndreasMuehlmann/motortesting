2d850d77bbf9e8d1b1abb1a9eb3e6bd8f2a43797
Andreas Muehlmann <andi.muehlmann@arcor.de>
2023-04-22 19:22:44 +0200

    main as class, voltage is reset

--- main.py
+++ main.py
@@ -5,35 +5,44 @@ from serial_interface import Serial_Interface
 from voltage_function import Voltage_Function
 
 
-def main():
-    fir_filter = Fir_Filter()
-    csv_writer = Csv_Writer('data.csv', ['time', 'rpm', 'filtered_rpm', 'voltage'])
-    voltage_function = Voltage_Function()
-    prev_measurement = 0
-    start_time = time.time()
-    while True:
-        try:
-            measurement = serial_interface.recv()
-            measurement = float(measurement)
-            prev_measurement = measurement
+class Motor_Testing:
+    def __init__(self):
+        self.digital_filter = Fir_Filter()
+        self.csv_writer = Csv_Writer('data.csv', ['time', 'rpm', 'filtered_rpm', 'voltage'])
+        self.voltage_function = Voltage_Function()
+        self.serial_interface = Serial_Interface()
+        self.serial_interface.flush()
+        self.prev_measurement = 0
+        self.start_time = time.time()
 
-        except (UnicodeDecodeError, ValueError):
-            measurement = prev_measurement
-        voltage = voltage_function.give_current_voltage()
-        serial_interface.send(voltage)
+    def run(self):
+        while True:
+            try:
+                measurement = self.serial_interface.recv()
+                measurement = float(measurement)
+                prev_measurement = measurement
 
-        filtered_measruement = fir_filter.give_filtered(measurement)
-        csv_writer.add_line_of_data(
-                [time.time() - start_time, measurement,
-                 filtered_measruement, voltage * 500 / 255]
-                )
-        time.sleep(0.005)
+            except (UnicodeDecodeError, ValueError):
+                measurement = prev_measurement
+            voltage = self.voltage_function.give_current_voltage()
+            self.serial_interface.send(voltage)
+
+            filtered_measruement = self.digital_filter.give_filtered(measurement)
+            self.csv_writer.add_line_of_data(
+                    [time.time() - self.start_time, measurement,
+                     filtered_measruement, voltage * 500 / 255]
+                    )
+            time.sleep(0.005)
+
+    def reset(self):
+        self.serial_interface.send(0)
+        self.serial_interface.reset()
 
 
 if __name__ == '__main__':
     try:
-        serial_interface = Serial_Interface()
-        serial_interface.flush()
-        main()
-    except KeyboardInterrupt:
-        serial_interface.reset()
+        motor_testing = Motor_Testing()
+        motor_testing.run()
+    except Exception as e:
+        motor_testing.reset()
+        print(e)
