f0abce3fbc945213674ea61796a24d3dbf06bb42
andi <andi.muehlmann@arcor.de>
2023-05-14 18:29:35 +0200

    correct .bat

--- run_motortesting.bat
+++ run_motortesting.bat
@@ -1,6 +1,6 @@
 @echo off
-".\C:\Users\diete\motortesting\pyvenv\Scripts\activate"
-"C:\Users\diete\AppData\Local\Programs\Python\Python311\python.exe" "C:\Users\diete\motortesting\live_plot.py" data.csv 200 &
-"C:\Users\diete\AppData\Local\Programs\Python\Python311\python.exe" "C:\Users\diete\motortesting\live_plot.py" parameters.csv 2000 &
-"C:\Users\diete\AppData\Local\Programs\Python\Python311\python.exe" "C:\Users\diete\motortesting\main.py"
+"C:\Users\diete\motortesting\pyvenv\Scripts\activate"
+python "C:\Users\diete\motortesting\live_plot.py" "C:\Users\diete\motortesting\data.csv" 200 &
+python "C:\Users\diete\motortesting\live_plot.py" "C:\Users\diete\motortesting\parameters.csv" 2000 &
+python "C:\Users\diete\motortesting\main.py"
 pause
