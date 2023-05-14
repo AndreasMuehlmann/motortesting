18a56db9cb5d18fcdbe4d4b36643c30575568cf1
Andreas Muehlmann <andi.muehlmann@arcor.de>
2023-05-14 18:37:22 +0200

    fix .bat

--- run_motortesting.bat
+++ run_motortesting.bat
@@ -1,6 +1,5 @@
 @echo off
-"C:\Users\diete\motortesting\pyvenv\Scripts\activate"
-python "C:\Users\diete\motortesting\live_plot.py" "C:\Users\diete\motortesting\data.csv" 200 &
-python "C:\Users\diete\motortesting\live_plot.py" "C:\Users\diete\motortesting\parameters.csv" 2000 &
-python "C:\Users\diete\motortesting\main.py"
+start "Data Plot" cmd /c "C:\Users\diete\motortesting\pyvenv\Scripts\activate && python C:\Users\diete\motortesting\live_plot.py C:\Users\diete\motortesting\data.csv 200"
+start "Parameter Plot" cmd /c "C:\Users\diete\motortesting\pyvenv\Scripts\activate && python C:\Users\diete\motortesting\live_plot.py C:\Users\diete\motortesting\parameters.csv 2000"
+start "Communication Arduino and Logic" cmd /c "C:\Users\diete\motortesting\pyvenv\Scripts\activate && python C:\Users\diete\motortesting\main.py"
 pause
