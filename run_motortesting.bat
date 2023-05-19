b3fdc032ab5e0c1c0f246e1686cefe1b584f2470
Andreas Muehlmann <andi.muehlmann@arcor.de>
2023-05-19 13:27:36 +0200

    fix batch

--- run_motortesting.bat
+++ run_motortesting.bat
@@ -1,5 +1,6 @@
 @echo off
 start /B "Data Plot" cmd /c "C:\Users\diete\motortesting\pyvenv\Scripts\activate && python C:\Users\diete\motortesting\live_plot.py C:\Users\diete\motortesting\data.csv 200"
 start /B "Parameter Plot" cmd /c "C:\Users\diete\motortesting\pyvenv\Scripts\activate && python C:\Users\diete\motortesting\live_plot.py C:\Users\diete\motortesting\parameters.csv 2000"
-"C:\Users\diete\motortesting\pyvenv\Scripts\activate && python C:\Users\diete\motortesting\main.py"
+call "C:\Users\diete\motortesting\pyvenv\Scripts\activate"
+python "C:\Users\diete\motortesting\main.py"
 pause
